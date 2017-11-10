659334d0319ac0788896e09882bed3d08cd60f88
Elliott Mitchell <ehem+android_kernel_lge_msm8996@m5p.com>
2017-11-10 13:27:00 -0800

    DirtySanta fixup v3, available via sysfs
    This version introduces a new interface for changing the crucial SMEM
    values.  The values can not be accessed via sysfs.  If the values aren't
    set via kernel command-line or kernel configuration, the values in sysfs
    will be writeable.  Beware the values MUST be set before the modem
    subsystem is started (pm-service); otherwise the modem firmware WILL
    panic, causing SSRs and often leading to kernel panic.
    
    On the intended target the key files appear as
    "/sys/devices/soc/2080000.qcom,mss/dirtysanta_lg_model_name"
    and "/sys/devices/soc/2080000.qcom,mss/dirtysanta_sim_num".
    
    Done for LineageOS as this type of interface is acceptable to LineageOS,
    whereas patching the kernel command-line in the boot image is not.

--- drivers/soc/qcom/pil-q6v5-mss.c
+++ drivers/soc/qcom/pil-q6v5-mss.c
@@ -39,6 +39,8 @@
 
 #include <linux/time.h>
 
+#include "dirtysanta_fixup.h"
+
 
 /* Added getting MSM chip version info, 2014-12-21, secheol.pyo@lge.com*/
 #define FEATURE_LGE_MODEM_LDB_EXCEPTION
@@ -534,6 +536,8 @@ static int pil_subsys_init(struct modem_data *drv,
 		goto err_ramdump;
 	}
 
+	dirtysanta_attach(&pdev->dev);
+
 	return 0;
 
 err_ramdump:
@@ -702,6 +706,8 @@ static int pil_mss_driver_probe(struct platform_device *pdev)
 static int pil_mss_driver_exit(struct platform_device *pdev)
 {
 	struct modem_data *drv = platform_get_drvdata(pdev);
+
+	dirtysanta_detach(&pdev->dev);
 
 	subsys_unregister(drv->subsys);
 	destroy_ramdump_device(drv->ramdump_dev);
